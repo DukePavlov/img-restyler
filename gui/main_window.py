ab9b40c9c31f32f48c5b5cdaede2945abfca7c5e
dpavlov-conga <dpavlov@conga.com>
2024-09-30 23:50:44 +0200

    refactoring_cleaning

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,21 +1,19 @@
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QLabel, QPushButton, QProgressBar, QCheckBox, QComboBox
-from PyQt5.QtGui import QPixmap, QImage
-from PyQt5.QtCore import QRect
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QLabel, QPushButton, QCheckBox, QComboBox
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QRect, Qt, QThread
 import sys
 import os
 import numpy as np
 import PIL
-import tensorflow as tf
 from PIL import Image
 from PIL.ImageQt import ImageQt
 from matplotlib import cm
-
-tf.compat.v1.enable_eager_execution()
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from segmentation.segmentator import Segmentator
 from style_transfer.restyler import Restyler
+from constants import MAX_ITER_NO, RestyleStatus, restyling_status_msg_map
 
 class ImageRestyler(QMainWindow):
     def __init__(self):
@@ -23,7 +21,7 @@ class ImageRestyler(QMainWindow):
         self.segmentator = Segmentator()
         self.restyler = Restyler()
         self.invert_mode = 0
-        self.train_iter_no = 10
+        self.train_iter_no = 2
         self.initUI()
 
     def initUI(self):
@@ -35,7 +33,6 @@ class ImageRestyler(QMainWindow):
 
         self.original_image = QLabel(self)
         self.original_image.setGeometry(QRect(50, 50, 250, 250))
-        self.original_image.setText("Original Image")
         self.original_image.setPixmap(QPixmap(None))
         self.original_image.setScaledContents(True)
         self.original_image.setObjectName("orig_img")
@@ -47,7 +44,6 @@ class ImageRestyler(QMainWindow):
 
         self.style_image = QLabel(self)
         self.style_image.setGeometry(QRect(350, 50, 250, 250))
-        self.style_image.setText("Style Image")
         self.style_image.setPixmap(QPixmap(None))
         self.style_image.setScaledContents(True)
         self.style_image.setObjectName("style_img")
@@ -59,7 +55,6 @@ class ImageRestyler(QMainWindow):
 
         self.original_image_mask = QLabel(self)
         self.original_image_mask.setGeometry(QRect(50, 330, 250, 250))
-        self.original_image_mask.setText("Original Image Mask")
         self.original_image_mask.setPixmap(QPixmap(None))
         self.original_image_mask.setScaledContents(True)
         self.original_image_mask.setObjectName("orig_img_mask")
@@ -80,11 +75,10 @@ class ImageRestyler(QMainWindow):
         self.result_image_label.setGeometry(QRect(540, 753, 400, 15))
         self.result_image_label.setText('Result Image')
 
-        self.prog_bar = QProgressBar(self)
-        self.prog_bar.setGeometry(600, 850, 300, 25)
-        self.prog_bar.setValue(0)
-        self.prog_bar.setFormat('Restyling progress')
-        self.prog_bar.setStyleSheet("background-color:#eaf2c9")
+        self.status_lbl = QLabel(self)
+        self.status_lbl.setGeometry(600, 850, 300, 25)
+        self.status_lbl.setAlignment(Qt.AlignCenter)
+        self.set_restyling_status(RestyleStatus.NOT_STARTED.value)
 
         self.btn_load = QPushButton('Load Original Image', self)
         self.btn_load.setGeometry(75, 700, 150, 40)
@@ -100,8 +94,8 @@ class ImageRestyler(QMainWindow):
 
         self.cb_train_iter_no = QComboBox(self)
         self.cb_train_iter_no.setGeometry(QRect(250, 750, 50, 20))
-        for k in range (2, 26):
-            self.cb_train_iter_no.addItem(str(k * 5))
+        for k in range (1, MAX_ITER_NO):
+            self.cb_train_iter_no.addItem(str(k))
         self.cb_train_iter_no.setCurrentIndex(1)
         self.cb_train_iter_no.currentIndexChanged.connect(self.train_iter_no_changed)
 
@@ -116,38 +110,44 @@ class ImageRestyler(QMainWindow):
         
         self.btn_style = QPushButton('Apply Style Transfer', self)
         self.btn_style.setGeometry(250, 800, 150, 40)
+        self.btn_style.clicked.connect(lambda: self.set_restyling_status(RestyleStatus.ACTIVE.value))
         self.btn_style.clicked.connect(self.style_transfer)
 
 
     def load_original_image(self):
-        fname = QFileDialog.getOpenFileName(self, 'Choose Original Image', '../images',"Image files (*.jpg *.png)")
+        fname = QFileDialog.getOpenFileName(self, 'Choose Original Image', '../images/animals',"Image files (*.jpg *.png)")
         self.original_image_path = fname[0]
         self.original_image.setPixmap(QPixmap(self.original_image_path))
         self.result_image.setPixmap(QPixmap(self.original_image_path))
         self.original_image_mask.setPixmap(QPixmap(None))
+        self.mask_image = None
+        self.mask_image_path = None
+        self.set_restyling_status(RestyleStatus.NOT_STARTED.value)
 
     
     def load_style_image(self):
-        fname = QFileDialog.getOpenFileName(self, 'Choose Style Image', '../images',"Image files (*.jpg *.png)")
+        fname = QFileDialog.getOpenFileName(self, 'Choose Style Image', '../images/styles',"Image files (*.jpg *.png)")
         self.style_image_path = fname[0]
         self.style_image.setPixmap(QPixmap(self.style_image_path))
+        self.set_restyling_status(RestyleStatus.NOT_STARTED.value)
 
 
-    def display_image(self, img_array):
-        height, width = img_array.shape[:2]
-        qimg = QImage(img_array.data, width, height, QImage.Format_Grayscale8)
-        pixmap = QPixmap.fromImage(qimg)
-        self.label.setPixmap(pixmap)
+    def set_restyling_status(self, restyle_status):
+        self.status_lbl.setText(restyling_status_msg_map[restyle_status][0])
+        self.status_lbl.setStyleSheet(f'background-color:{restyling_status_msg_map[restyle_status][1]}')
+        self.restyle_status = restyle_status
+
 
     def update_invert_mode(self, checked):
         if checked:
             self.invert_mode = 1
         else:
             self.invert_mode = 0
-    
+
 
     def train_iter_no_changed(self, i):
-        print(int(self.cb_train_iter_no.itemText(i)))
+        self.train_iter_no = int(self.cb_train_iter_no.itemText(i))
+
 
     def segment_image(self):
         self.segmentator.load_model()
@@ -162,17 +162,18 @@ class ImageRestyler(QMainWindow):
 
 
     def style_transfer(self):
+        self.set_restyling_status(RestyleStatus.ACTIVE.value)
+        QApplication.processEvents()
+        QThread.sleep(1)
         self.restyler.add_content_image(self.original_image_path)
         self.restyler.add_style_image(self.style_image_path)
         self.restyler.add_mask_image(self.mask_image_path)
         self.result_image_path = '../images/result.png'
-        result_image = self.restyler.restyle()
+        result_image = self.restyler.restyle(self.train_iter_no, self.invert_mode)
         result_image = (result_image.squeeze()*255).astype(np.uint8)
         im = PIL.Image.fromarray(result_image, mode='RGB')
         im.save(self.result_image_path)
         qimage = ImageQt(im)
         pixmap = QPixmap.fromImage(qimage)
         self.result_image.setPixmap(pixmap)
-
-    def retrain_model(self):
-        pass
+        self.set_restyling_status(RestyleStatus.FINISHED.value)
